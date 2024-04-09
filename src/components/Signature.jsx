d7160b422b37b12cb2255694b2eafa09e4817c30
jay suthar <jay37192@gmail.com>
2024-04-09 18:09:03 +0530

    #

--- src/components/Signature.jsx
+++ src/components/Signature.jsx
@@ -27,8 +27,8 @@ const Signature = () => {
   const contactImage = isLightMode ? contactDark : contactLight;
 
   return (
-    <div className={`relative lg:w-full md:w-full md:h-[500px] mx:w-full mx:h-[606px] sm:w-[110%] sm:h-[490px] small:w-full small:h-[790px] ${isLightMode ? 'bg-white' : 'bg-[#000000]'}`}>
-      <div className={`xl:px-9 xl:py-6 lg:px-5 lg:py-3 mx:px-4 absolute right-6 bottom-6 flex top-[6%] left-[2%]`}>
+    <div className={`relative lg:w-full md:w-full md:h-[500px] mx:w-full mx:h-[606px] sm:w-[110%] sm:h-[490px] small:w-full small:h-[608px] ${isLightMode ? 'bg-white' : 'bg-[#000000]'}`}>
+      <div className={`xl:px-9 xl:py-6 lg:px-5 lg:py-3 mx:px-4 absolute right-6 bottom-6 flex top-[0%] left-[2%]`}>
         <div className='xl:pt-9 xl:pl-40 sxl:pt-9 sxl:pl-20 lg:pt-9 lg:pl-14 md:pt-9 md:pl-10 sm:pt-9 sm:pl-9 mx:pt-7 mx:pl-7 small:pt-14 small:pl-5'>
           <img src={logoImage} alt="logo" className='xl:w-[200px] lg:w-[180px] xl:h-[200px] lg:h-[180px] sm:w-[150px] sm:h-[150px] mx:w-32 mx:h-32' />
         </div>
@@ -51,8 +51,8 @@ const Signature = () => {
           </div>
         </div>
       </div>
-      <div className={`border-b ${isLightMode ? 'border-black' : 'border-white'} opacity-50 h-1 md:w-[80%] sm:w-[80%] mx:w-[90%] absolute md:top-[68%] md:left-[10%] sm:top-[73%] mx:top-[67%] small:top-[63%] small:w-[90%] small:left-6 sm:left-[7%] `} />
-      <div className='absolute lg:top-[75%] lg:left-[7%] sxl:left-[13%] xl:left-[21%] md:top-[73%] md:left-[6%] items-center lg:w-[55.2rem] md:w-[43rem] sm:top-[77%] sm:left-[7%] sm:w-[40rem] mx:top-[70%] small:top-[68%] mx:px-4 small:px-6 sm:px-0'>
+      <div className={`border-b ${isLightMode ? 'border-black' : 'border-white'} opacity-50 h-1 md:w-[80%] sm:w-[80%] mx:w-[90%] absolute md:top-[68%] md:left-[10%] sm:top-[73%] mx:top-[67%] small:top-[68%] small:w-[90%] small:left-6 sm:left-[7%] `} />
+      <div className='absolute lg:top-[75%] lg:left-[7%] sxl:left-[13%] xl:left-[21%] md:top-[73%] md:left-[6%] items-center lg:w-[55.2rem] md:w-[43rem] sm:top-[77%] sm:left-[7%] sm:w-[40rem] mx:top-[70%] small:top-[72%] mx:px-4 small:px-6 sm:px-0'>
         <p className={`text-${isLightMode ? 'black' : 'white'} text-[15px] font-light text-center font`}><span className={`text-${isLightMode ? 'black' : 'white'} font-semibold`}>CONFIDENTIALITY NOTICE:</span>This email was sent to you as you are registered member of Codage Corporation LTD.
           Please do not reply to this email. Emails sent to this address will not be answered.</p>
       </div>
